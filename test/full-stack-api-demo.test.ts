025cf098d98a9bb6c55a849f428bd9e8a3472799
Ash R <sureshramayah@gmail.com>
2023-09-24 17:39:21 +1000

    Add files via upload

---
+++ test/full-stack-api-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FullStackApiDemo from '../lib/full-stack-api-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/full-stack-api-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FullStackApiDemo.FullStackApiDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
