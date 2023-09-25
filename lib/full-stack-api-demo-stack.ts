aebb9d29315ef4edfcbc3dffd01ceae55c326d97
Ash R <sureshramayah@gmail.com>
2023-09-25 19:29:04 +1000

    Add files via upload

--- lib/full-stack-api-demo-stack.ts
+++ lib/full-stack-api-demo-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { S3ToLambdaProps, S3ToLambda } from '@aws-solutions-constructs/aws-s3-lambda';
+import { CloudFrontToS3 } from '@aws-solutions-constructs/aws-cloudfront-s3';
+
+/*
+// AWS Solution construct to deploy to S3
+*/
 
 export class FullStackApiDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new S3ToLambda(this, 'test-s3-lambda', {
+      lambdaFunctionProps: {
+        code: lambda.Code.fromAsset(`${__dirname}/LAMBDA3.zip`),
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: 'index.handler'
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FullStackApiDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CloudFrontToS3(this, 'test-cloudfront-s3', {
+      // code to deploy S3 to cloudfront goes here
+
+    });
+
   }
 }
